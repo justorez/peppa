bc5211d484f8217a6970e96c782a6dd81c7b6d4e
justorez <alonezero@foxmail.com>
2024-02-13 15:11:53 +0800

    chore(type): update auto imports

--- types/components.d.ts
+++ types/components.d.ts
@@ -8,8 +8,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     'IMdi:check': typeof import('~icons/mdi/check')['default']
-    'IMdi:chevronLeft': typeof import('~icons/mdi/chevron-left')['default']
-    'IMdi:chevronRight': typeof import('~icons/mdi/chevron-right')['default']
     'IMdi:close': typeof import('~icons/mdi/close')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
